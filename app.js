c05ad44981fc281b753eca56fb22cebb848cf824
task11 <task11@naver.com>
2021-12-15 19:37:06 +0900

    Day3 - function

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const x // constant
+const player = {
+  name: "nico",
+  sayHello: function () {
+    console.log("hello");
+  },
+}
+
+player.sayHello();
+
+const a = parseInt("5") + 3;
+const b = "aa"
+console.log(isNaN(a));
+
+console.log(isNaN(b));
