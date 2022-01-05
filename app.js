b30e97d44f7a3d3b645a2c00474941521fc8848d
task11 <task11@naver.com>
2022-01-05 21:31:17 +0900

    [Learn] DOC & WINDOW Event

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-const title = document.querySelector(".hello h1");
+const title = document.querySelector("div.hello:first-child h1");
 
+function handleTitleClick() {
+  title.style.color = "blue"
+}
+
+function handleMouseEnter() {
+  title.style.color = "blue"
+}
+function handleMouseLeave() {
+  title.style.color = "red"
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("Copy 하지마")
+}
+
+function handleWindowOffline() {
+  alert("NO WIFI~!!!")
+}
+
+function handleWindowOnline() {
+  alert("WIFI GOOD")
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
