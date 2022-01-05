928fbee24b75d6bffb494121e56df6c8a5ea7824
task11 <task11@naver.com>
2022-01-05 21:37:11 +0900

    [Learn] DOC & WINDOW Event Condition

--- app.js
+++ app.js
@@ -1,39 +1,18 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  title.style.color = "blue"
+  const currentColor = h1.style.color;
+  let newColor;
+
+  if (currentColor === "blue") {
+    newColor = "red";
+  } else {
+    newColor = "blue";
+  }
+
+  h1.style.color = newColor;
+
+
 }
 
-function handleMouseEnter() {
-  title.style.color = "blue"
-}
-function handleMouseLeave() {
-  title.style.color = "red"
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("Copy 하지마")
-}
-
-function handleWindowOffline() {
-  alert("NO WIFI~!!!")
-}
-
-function handleWindowOnline() {
-  alert("WIFI GOOD")
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
