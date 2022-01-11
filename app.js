f2e661e2502a720e1ceccb5d316a4385860b5bcd
task11 <task11@naver.com>
2022-01-11 22:39:47 +0900

    [Learn] ClassList Toggle

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
   const activeClass = "active";
-  if (h1.classList.contains(activeClass)) {
-    h1.classList.remove(activeClass);
-  } else {
-    h1.classList.add(activeClass);
-  }
-
+  h1.classList.toggle(activeClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
