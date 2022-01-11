debab5d83a03a849a251d7e50e4ae1d5691df355
task11 <task11@naver.com>
2022-01-11 22:37:00 +0900

    [Learn] className & classList

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-  const currentColor = h1.style.color;
-  let newColor;
-
-  if (currentColor === "blue") {
-    newColor = "red";
+  const activeClass = "active";
+  if (h1.classList.contains(activeClass)) {
+    h1.classList.remove(activeClass);
   } else {
-    newColor = "blue";
+    h1.classList.add(activeClass);
   }
-
-  h1.style.color = newColor;
-
 
 }
 
 h1.addEventListener("click", handleTitleClick);
-
-
-
