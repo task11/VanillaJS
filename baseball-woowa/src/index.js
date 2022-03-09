bad59d2c272719864cfd2a207a06143d2190c82d
task11 <task11@naver.com>
2022-03-09 22:36:02 +0900

    feat(baseball-woowa): 플레이어가 잘못된 값 입력 시 error 출력

--- baseball-woowa/src/index.js
+++ baseball-woowa/src/index.js
@@ -1,6 +1,7 @@
 export default class BaseballGame {
 
   constructor() {
+    this.CORRECT = `🎉정답을 맞추셨습니다.🎉 \n 게임을 새로 시작하시겠습니까?`;
     this.getRandomNumber();
     this.getSelector('.game-form').addEventListener('submit', (e) => this.clickSubmit(e, this._computerInput));
     console.log(this._computerInput);
@@ -9,7 +10,6 @@ export default class BaseballGame {
   play(computerInputNumbers, userInputNumbers) {
     this._computerInput = computerInputNumbers;
     const result = this.compareNumber(String(this._computerInput), String(userInputNumbers));
-    console.log(result.comment);
     return result;
   }
 
@@ -29,9 +29,16 @@ export default class BaseballGame {
   clickSubmit(e, computerInput) {
     e.preventDefault();
     const userInput = e.target.querySelector('#user-input');
-    const result = this.play(computerInput, userInput.value);
-    this.renderResult(result);
+    const errorFlag = this.catchInputError(userInput.value);
+
+    if (!errorFlag) {
+      const result = this.play(computerInput, userInput.value);
+      this.renderResult(result);
+    } else {
+      this.renderResult('');
+    }
     userInput.value = "";
+
   }
 
   compareNumber(answer, input) {
@@ -48,28 +55,41 @@ export default class BaseballGame {
   }
 
   makeResult(strike, ball) {
-    let result = {
-      comment: '',
-      restart: false
-    };
-    if (strike === 3) {
-      result.comment = `🎉정답을 맞추셨습니다.🎉 \n 게임을 새로 시작하시겠습니까?`;
-      result.restart = true;
-    }
-    else if (strike > 0 && ball > 0) result.comment = `${ball}볼 ${strike}스트라이크`;
-    else if (ball > 0 && strike === 0) result.comment = `${ball}볼`;
-    else if (strike > 0 && ball === 0) result.comment = `${strike}스트라이크`;
-    else result.comment = '낫싱';
+    let CORRECT = this.CORRECT;
+    let result = '';
+    if (strike === 3) result = CORRECT;
+    else if (strike > 0 && ball > 0) result = `${ball}볼 ${strike}스트라이크`;
+    else if (ball > 0 && strike === 0) result = `${ball}볼`;
+    else if (strike > 0 && ball === 0) result = `${strike}스트라이크`;
+    else result = '낫싱';
     return result;
   }
 
   renderResult(result) {
+    let CORRET = this.CORRECT;
     const $result = this.getSelector('#result');
     const $restart = this.getSelector('#game-restart-button');
-    $result.innerText = result.comment;
-    if (result.restart === true) {
+    $result.innerText = result;
+    if (result === CORRET) {
       $restart.style.display = 'block';
     }
+  }
+
+  catchInputError(userInput) {
+    let errorFlag = false;
+    const inputLength = userInput.length;
+    const inputSet = new Set(userInput);
+    const inputSetLength = inputSet.size;
+
+    if (userInput === "" || userInput.length > 3) {
+      alert('숫자를 1개 이상 3개 이하로 입력해주세요.');
+      errorFlag = true;
+    }
+    else if (inputLength !== inputSetLength) {
+      alert('중복된 숫자를 입력할 수 없습니다.');
+      errorFlag = true;
+    }
+    return errorFlag;
   }
 }
 
