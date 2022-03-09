faeaa1a22e0d2e1ef199524102dc14efd0f10bfd
task11 <task11@naver.com>
2022-03-09 21:41:28 +0900

    feat(baseball-woowa): 플레이어와 컴퓨터의 값 비교 기능

--- baseball-woowa/src/index.js
+++ baseball-woowa/src/index.js
@@ -1,23 +1,68 @@
 export default class BaseballGame {
+
+  constructor() {
+    this.getComputerNumber();
+    this.getSelector('.game-form').addEventListener('submit', (e) => this.clickSubmit(e, this._computerInput));
+  }
+
   play(computerInputNumbers, userInputNumbers) {
-    console.log(computerInputNumbers);
-    return "결과 값 String";
+    this._computerInput = computerInputNumbers;
+    const result = this.compareNumber(String(this._computerInput), String(userInputNumbers));
+    console.log(result);
+    return result;
   }
 
   getComputerNumber() {
-    let computerNumber = ""
-    for (let i = 0; i < 3; i++) {
-      computerNumber += MissionUtils.Random.pickNumberInRange(1, 9);
+    let computerNumber = new Set();
+    for (let i = 0; computerNumber.size < 3; i++) {
+      let num = MissionUtils.Random.pickNumberInRange(1, 9)
+      computerNumber.add(num);
     }
-    return computerNumber;
+    this._computerInput = Array.from(computerNumber).join('');
   }
 
+  getSelector(target) {
+    return document.querySelector(target);
+  }
 
+  clickSubmit(e, computerInput) {
+    e.preventDefault();
+    const userInput = e.target.querySelector('#user-input');
+    const result = this.play(computerInput, userInput.value);
+
+    userInput.value = "";
+  }
+
+  compareNumber(answer, input) {
+    let strike = 0;
+    let ball = 0;
+    for (let i = 0; i < answer.length; i++) {
+      if (answer[i] == input[i]) {
+        strike++;
+      } else if (answer.includes(input[i])) {
+        ball++;
+      }
+    }
+    return this.makeResult(strike, ball);
+  }
+
+  makeResult(strike, ball) {
+    let result = '';
+    if (strike > 0 && ball > 0) {
+      result = `${ball}볼 ${strike}스트라이크`;
+    } else if (ball > 0 && strike === 0) {
+      result = `${ball}볼`;
+    } else if (strike > 0 && ball === 0) {
+      result = `${strike}스트라이크`
+    } else {
+      result = '낫싱';
+    }
+    return result;
+  }
 }
 
+new BaseballGame();
 
-
-const game = new BaseballGame();
-const answer = game.getComputerNumber();
-// 예시
-game.play(answer, 456); // '낫싱'
+// const game = new BaseballGame();
+// // 예시
+// game.play(123, 326); // '낫싱'
