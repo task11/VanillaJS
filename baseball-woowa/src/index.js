ff71b9267c6d2ab596777571f7953a0b4bc4b619
task11 <task11@naver.com>
2022-03-09 22:10:17 +0900

    feat(baseball-woowa): 플레이어 입력 결과 값 렌더링

--- baseball-woowa/src/index.js
+++ baseball-woowa/src/index.js
@@ -1,18 +1,19 @@
 export default class BaseballGame {
 
   constructor() {
-    this.getComputerNumber();
+    this.getRandomNumber();
     this.getSelector('.game-form').addEventListener('submit', (e) => this.clickSubmit(e, this._computerInput));
+    console.log(this._computerInput);
   }
 
   play(computerInputNumbers, userInputNumbers) {
     this._computerInput = computerInputNumbers;
     const result = this.compareNumber(String(this._computerInput), String(userInputNumbers));
-    console.log(result);
+    console.log(result.comment);
     return result;
   }
 
-  getComputerNumber() {
+  getRandomNumber() {
     let computerNumber = new Set();
     for (let i = 0; computerNumber.size < 3; i++) {
       let num = MissionUtils.Random.pickNumberInRange(1, 9)
@@ -29,7 +30,7 @@ export default class BaseballGame {
     e.preventDefault();
     const userInput = e.target.querySelector('#user-input');
     const result = this.play(computerInput, userInput.value);
-
+    this.renderResult(result);
     userInput.value = "";
   }
 
@@ -47,17 +48,28 @@ export default class BaseballGame {
   }
 
   makeResult(strike, ball) {
-    let result = '';
-    if (strike > 0 && ball > 0) {
-      result = `${ball}볼 ${strike}스트라이크`;
-    } else if (ball > 0 && strike === 0) {
-      result = `${ball}볼`;
-    } else if (strike > 0 && ball === 0) {
-      result = `${strike}스트라이크`
-    } else {
-      result = '낫싱';
+    let result = {
+      comment: '',
+      restart: false
+    };
+    if (strike === 3) {
+      result.comment = `🎉정답을 맞추셨습니다.🎉 \n 게임을 새로 시작하시겠습니까?`;
+      result.restart = true;
     }
+    else if (strike > 0 && ball > 0) result.comment = `${ball}볼 ${strike}스트라이크`;
+    else if (ball > 0 && strike === 0) result.comment = `${ball}볼`;
+    else if (strike > 0 && ball === 0) result.comment = `${strike}스트라이크`;
+    else result.comment = '낫싱';
     return result;
+  }
+
+  renderResult(result) {
+    const $result = this.getSelector('#result');
+    const $restart = this.getSelector('#game-restart-button');
+    $result.innerText = result.comment;
+    if (result.restart === true) {
+      $restart.style.display = 'block';
+    }
   }
 }
 
